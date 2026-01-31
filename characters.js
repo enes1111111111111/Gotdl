e7ad6f0c43fd219da8ae3b7bff9fa9c52a307656
enes1111111111111 <eneeesuslu01@gmail.com>
2026-02-01 02:45:44 +0300

    Add files via upload

---
+++ characters.js
@@ -0,0 +1,26 @@
+const characters = [
+  {
+    name: "Tyrion Lannister",
+    house: "Lannister",
+    gender: "Male",
+    age: 32,
+    origin: "Casterly Rock",
+    image: "https://raw.githubusercontent.com/enes1111111111111/Gotdl-images/main/tyrion.jpg"
+  },
+  {
+    name: "Jon Snow",
+    house: "Stark",
+    gender: "Male",
+    age: 25,
+    origin: "Winterfell",
+    image: "https://raw.githubusercontent.com/enes1111111111111/Gotdl-images/main/images%20(3).jpeg"
+  },
+  {
+    name: "Daenerys Targaryen",
+    house: "Targaryen",
+    gender: "Female",
+    age: 24,
+    origin: "Dragonstone",
+    image: "https://raw.githubusercontent.com/enes1111111111111/Gotdl-images/main/images%20(5).jpeg"
+  }
+];
